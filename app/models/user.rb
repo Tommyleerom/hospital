4ca5b6afe5ba2c4621e8785a3ff2fa3e923029ff
Tommyleerom <tommyleerom@gmail.com>
2022-02-21 15:59:33 +0300

    fix name in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+  validates :name, presence: true
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
 end

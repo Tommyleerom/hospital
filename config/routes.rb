fd503500e20d897cbe97561b2870eb6ed65c6179
tommyleerom <tommyleerom@gmail.com>
2022-02-23 21:20:05 +0300

    Add devise doctor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", confirmations: "users/confirmations", omniauth: "users/omniauth", passwords: "users/passwords", registrations: "users/registrations", unlocks: "users/unlocks"}
+ 
+  devise_for :doctors, path: 'doctors', controllers: { sessions: "doctors/sessions", confirmations: "doctors/confirmations", omniauth: "doctors/omniauth", passwords: "doctors/passwords", registrations: "doctors/registrations", unlocks: "doctors/unlocks"}
+
   root 'pages#home'  
-  devise_for :users
 
 end
